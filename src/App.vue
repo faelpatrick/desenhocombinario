adbd4061d77564452ea43f9fdcb0733e3455d0bd
Rafael Patrick de Souza <rafael.souza@navarraaluminio.com>
2024-03-08 16:49:03 +0000

    My First Commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-text-field
+          v-for="(input, index) in binaryInputs"
+          :key="index"
+          v-model="binaryInputs[index]"
+          :maxlength="8"
+          :rules="[rules]"
+          label="Insira os binários"
+          variant="underlined"
+          dense
+        ></v-text-field>
+      </v-col>
+      <v-col cols="8">
+        <div class="grid">
+          <div
+            v-for="(row, rowIndex) in binaryInputs"
+            :key="`row-${rowIndex}`"
+            class="row"
+          >
+            <div
+              v-for="(bit, columnIndex) in row.split('')"
+              :key="`col-${columnIndex}`"
+              :class="{'bit': true, 'on': bit === '1'}"
+            ></div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const binaryInputs = ref(Array(8).fill(''))
+
+  const rules = [
+    value => value.length <= 8 || 'Máximo 8 dígitos',
+    value => /^([01]{0,8})$/.test(value) || 'Somente 0s e 1s são permitidos',
+  ]
+</script>
+
+<style>
+  .grid .row {
+    display: flex;
+  }
+
+  .bit {
+    width: 65px;
+    height: 65px;
+    border: 1px solid #000;
+  }
+
+  .on {
+    background-color: #000;
+  }
+</style>
